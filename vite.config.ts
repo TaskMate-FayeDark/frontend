464a1feead3e61e19fcd43eb0483b7d0201eedf3
devhatebug <togiap.dev@gmail.com>
2024-11-18 16:04:01 +0700

    config vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-export default defineConfig({
+export default defineConfig (() => {
+  const envDir = './env';
+  return{
   plugins: [react(), TanStackRouterVite()],
+    envDir: envDir,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}})
