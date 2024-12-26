215c862baef717218fe8382da54103d3f5be4c01
devhatebug <togiap.dev@gmail.com>
2024-12-26 11:00:03 +0700

    add CalendarPage component and integrate into work area router

--- src/app/work-area/work-area-router.tsx
+++ src/app/work-area/work-area-router.tsx
@@ -3,6 +3,7 @@ import { rootRoute } from "../router.tsx";
 import { WorkAreaLayout } from "./work-area-layout.tsx";
 import { BoardList } from "./boards/board.tsx";
 import BoardIndex from "./boards/board-index/index.tsx";
+import CalendarPage from "./calendar/index.tsx";
 
 const _workAreaRoute = createRoute({
   getParentRoute: () => rootRoute,
@@ -20,5 +21,10 @@ export const workAreaRouter = _workAreaRoute.addChildren([
     getParentRoute: () => _workAreaRoute,
     path: "/boards/$id",
     component: BoardIndex,
+  }),
+  createRoute({
+    getParentRoute: () => _workAreaRoute,
+    path: "/calendar",
+    component: CalendarPage,
   }),
 ]);
